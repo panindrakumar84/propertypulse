439edeee4dd530e6f67e179554ac763ae341ecdf
panindrakumar84 <bphani678@gmail.com>
2026-02-16 13:44:10 +0530

    fix:middlware config

--- middleware.js
+++ middleware.js
@@ -1,4 +1,7 @@
-export { default } from "next-auth/middleware";
+import NextAuth from "next-auth";
+import { authOptions } from "./utils/authOptions";
+
+export default NextAuth(authOptions).auth;
 
 export const config = {
   matcher: ["/properties/add", "/profile", "/properties/saved", "/messages"],
